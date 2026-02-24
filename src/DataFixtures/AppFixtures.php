9dc120ea1952ed6b028252965595176af40b6056
CalebValls <calebvallsj@gmail.com>
2026-02-24 16:42:20 +0100

    AppFixtures done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Task;
+use App\Model\TaskStatusEnum;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +13,18 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        // task1
+        $task1 = new Task();
+        $task1 -> setName('aprender doctrine');
+        $task1 -> setTime('2 dias');
+        $task1 -> setStatus(TaskStatusEnum:: EMPEZADA);
+        $manager -> persist($task1);
+
+        $task2 = new Task();
+        $task2 -> setName('aprender bases');
+        $task2 -> setTime('1 dia');
+        $task2 -> setStatus(TaskStatusEnum:: TERMINADA);
+        $manager -> persist($task2);
 
         $manager->flush();
     }
