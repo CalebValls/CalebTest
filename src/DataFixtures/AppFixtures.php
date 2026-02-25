39559e457e32aeca839cf50bec72d77fe7e52dd9
CalebValls <calebvallsj@gmail.com>
2026-02-25 11:28:02 +0100

    foundry done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,33 +5,18 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Task;
+use App\Factory\TaskFactory;
 use App\Model\TaskStatusEnum;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        // task1
-        $task1 = new Task();
-        $task1->setName('aprender doctrine');
-        $task1->setTime('2 dias');
-        $task1->setStatus(TaskStatusEnum::EMPEZADA);
-        $manager->persist($task1);
-
-        $task2 = new Task();
-        $task2->setName('aprender bases');
-        $task2->setTime('1 dia');
-        $task2->setStatus(TaskStatusEnum::TERMINADA);
-        $manager->persist($task2);
-
-
-        $task3 = new Task();
-        $task3->setName('prueba');
-        $task3->setTime('10min');
-        $task3->setStatus(TaskStatusEnum::SIN_EMPEZAR);
-        $manager->persist($task3);
-        $manager->flush();
-    }
+TaskFactory::createOne( [
+    'name' => 'aprender doctrine',
+    'time' => '2 dias',
+    'status' => TaskStatusEnum::EMPEZADA,
+]);
+TaskFactory::createMany(5);
+}
 }
